d441aab79f4d7790bfd27dc4bed733a20ac90b6b
Николай <n.bobrovnik@aic.ru>
2019-04-06 04:24:36 +0300

    Updates

---
+++ precache-manifest.a748721f989bf4f17fc7c7a9b0e9d55a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fe35fff68339504b40ea",
+    "url": "/gh-pages/static/js/runtime~main.0116bf16.js"
+  },
+  {
+    "revision": "08d98d224e4e6b2a14f8",
+    "url": "/gh-pages/static/js/main.73a13a11.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/gh-pages/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "08d98d224e4e6b2a14f8",
+    "url": "/gh-pages/static/css/main.e56b5b31.chunk.css"
+  },
+  {
+    "revision": "88213c59c96dac3019861d4dd0c1759e",
+    "url": "/gh-pages/index.html"
+  }
+];
